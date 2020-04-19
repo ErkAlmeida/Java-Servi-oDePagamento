e812c5142649ebe4e2eebf76d5fe2c7d03d58def
Erick Almeida <erickprofissional@gmail.com>
2020-04-19 19:27:21 -0300

    Estrura das Classes montada

--- src/model/entities/Contract.java
+++ src/model/entities/Contract.java
@@ -2,36 +2,70 @@ package model.entities;
 
 import java.util.Date;
 
+import model.services.TaxServices;
+
 
 public class Contract {
 
 	private Date dateContract;
 	private Double valorContract;
+	private Integer numberMonths;
+	
+	public TaxServices taxServices;
 	
 	public Contract(){}
 	
-	public Contract(Date dateContract, Double valorContract) 
-	{
+	
+	public Contract(Date dateContract, Double valorContract, Integer numberMonths, TaxServices taxServices) {
+
 		this.dateContract = dateContract;
 		this.valorContract = valorContract;
+		this.numberMonths = numberMonths;
+		this.taxServices = taxServices;
 	}
-	
-	public Date getDateContract () {
-		
+
+	public Date getDateContract() {
 		return dateContract;
 	}
-	
+
+
 	public void setDateContract(Date dateContract) {
 		this.dateContract = dateContract;
 	}
+
 
 	public Double getValorContract() {
 		return valorContract;
 	}
 
+
 	public void setValorContract(Double valorContract) {
 		this.valorContract = valorContract;
 	}
-	
-	
+
+
+	public Integer getNumberMonths() {
+		return numberMonths;
+	}
+
+
+	public void setNumberMonths(Integer numberMonths) {
+		this.numberMonths = numberMonths;
+	}
+
+
+	public TaxServices getTaxServices() {
+		return taxServices;
+	}
+
+
+	public void setTaxServices(TaxServices taxServices) {
+		this.taxServices = taxServices;
+	}
+
+
+	public Double processContract() {
+		return 0.0;
+		
+	}
 }
---
+++ src/model/services/taxPaypail.java
@@ -0,0 +1,9 @@
+package model.services;
+
+public class taxPaypail implements TaxServices{
+	
+	public Double tax(double amount, int numberMonths) {
+		return 0.0;
+	}
+
+}
